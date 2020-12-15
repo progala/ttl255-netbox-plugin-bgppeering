6c868b0f56bb1862f97b14be8e1382dfdd596eed
progala <progala@progala.net>
2020-12-15 19:48:22 +0000

    Add url for Bgp Peering view

--- netbox_bgppeering/urls.py
+++ netbox_bgppeering/urls.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.urls import path
 
+from .views import BgpPeeringView
+
 
 def dummy_view(request):
     html = "<html><body>BGP Peering plugin.</body></html>"
@@ -9,4 +11,5 @@ def dummy_view(request):
 
 urlpatterns = [
     path("", dummy_view, name="bgppeering_list"),
+    path("<int:pk>/", BgpPeeringView.as_view(), name="bgppeering"),
 ]
