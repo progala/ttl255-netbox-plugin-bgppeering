eb0ee58ceabcf3040411e34e05761a7d770272f1
progala <progala@progala.net>
2020-12-16 21:38:57 +0000

    Add 'add/' endpoint

--- netbox_bgppeering/urls.py
+++ netbox_bgppeering/urls.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.urls import path
 
-from .views import BgpPeeringView, BgpPeeringListView
+from .views import BgpPeeringCreateView, BgpPeeringView, BgpPeeringListView
 
 
 urlpatterns = [
     path("", BgpPeeringListView.as_view(), name="bgppeering_list"),
     path("<int:pk>/", BgpPeeringView.as_view(), name="bgppeering"),
+    path("add/", BgpPeeringCreateView.as_view(), name="bgppeering_add"),
 ]
