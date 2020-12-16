4cf5ad95a368514bf065ec6f5b4e91d773a39e1a
progala <progala@progala.net>
2020-12-16 21:38:06 +0000

    Added 'add' button to plugin menu

--- netbox_bgppeering/navigation.py
+++ netbox_bgppeering/navigation.py
@@ -1,9 +1,18 @@
-from extras.plugins import PluginMenuItem
+from extras.plugins import PluginMenuButton, PluginMenuItem
+from utilities.choices import ButtonColorChoices
 
 
 menu_items = (
     PluginMenuItem(
         link="plugins:netbox_bgppeering:bgppeering_list",
         link_text="BGP Peerings",
+        buttons=(
+            PluginMenuButton(
+                link="plugins:netbox_bgppeering:bgppeering_add",
+                title="Add",
+                icon_class="fa fa-plus",
+                color=ButtonColorChoices.GREEN,
+            ),
+        ),
     ),
 )
