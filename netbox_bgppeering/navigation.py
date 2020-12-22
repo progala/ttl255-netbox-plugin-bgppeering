1701db4f0b129d5e251a0d37d2de8a89d5957f18
progala <progala@progala.net>
2020-12-22 11:41:24 +0000

    NB 2.10+ uses material design, old versions use font awesome

--- netbox_bgppeering/navigation.py
+++ netbox_bgppeering/navigation.py
@@ -1,6 +1,13 @@
 from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
+from .release import NETBOX_RELEASE_CURRENT, NETBOX_RELEASE_210
+
+
+if NETBOX_RELEASE_CURRENT >= NETBOX_RELEASE_210:
+    icon_class_plus = "mdi mdi-plus-thick"
+else:
+    icon_class_plus = "fa fa-plus"
 
 menu_items = (
     PluginMenuItem(
@@ -10,7 +17,7 @@ menu_items = (
             PluginMenuButton(
                 link="plugins:netbox_bgppeering:bgppeering_add",
                 title="Add",
-                icon_class="fa fa-plus",
+                icon_class=icon_class_plus,
                 color=ButtonColorChoices.GREEN,
             ),
         ),
