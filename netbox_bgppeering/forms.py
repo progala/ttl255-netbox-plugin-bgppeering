d7b5de7efcf1674267b134fffa076ae7f0fd1c42
progala <progala@progala.net>
2020-12-28 21:01:40 +0000

    Add BgpPeeringFilter form

--- netbox_bgppeering/forms.py
+++ netbox_bgppeering/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from dcim.models import Device, Site
 from utilities.forms import BootstrapMixin
 
 from .models import BgpPeering
@@ -20,3 +21,35 @@ class BgpPeeringForm(BootstrapMixin, forms.ModelForm):
             "remote_ip",
             "description",
         ]
+
+
+class BgpPeeringFilterForm(BootstrapMixin, forms.ModelForm):
+    """Form for filtering BgpPeering instances."""
+
+    q = forms.CharField(required=False, label="Search")
+
+    site = forms.ModelChoiceField(
+        queryset=Site.objects.all(), required=False, to_field_name="slug"
+    )
+
+    device = forms.ModelChoiceField(
+        queryset=Device.objects.all(),
+        to_field_name="name",
+        required=False,
+    )
+
+    local_as = forms.IntegerField(
+        required=False,
+    )
+
+    remote_as = forms.IntegerField(
+        required=False,
+    )
+
+    peer_name = forms.CharField(
+        required=False,
+    )
+
+    class Meta:
+        model = BgpPeering
+        fields = []
