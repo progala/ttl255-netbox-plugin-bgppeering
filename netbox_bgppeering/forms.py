6b83924a754bbca9e395a47ec2ad442e85724445
progala <progala@progala.net>
2020-12-29 19:24:40 +0000

    Check if local and remote IPs are in the same network

--- netbox_bgppeering/forms.py
+++ netbox_bgppeering/forms.py
@@ -1,3 +1,5 @@
+import ipaddress
+
 from django import forms
 
 from dcim.models import Device, Site
@@ -21,6 +23,21 @@ class BgpPeeringForm(BootstrapMixin, forms.ModelForm):
             "remote_ip",
             "description",
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        local_ip = cleaned_data.get("local_ip")
+        remote_ip = cleaned_data.get("remote_ip")
+
+        if local_ip and remote_ip:
+            # We can trust these are valid IP addresses. Format validation done in .super()
+            if (
+                ipaddress.ip_interface(str(local_ip)).network
+                != ipaddress.ip_interface(str(remote_ip)).network
+            ):
+                msg = "Local IP and Remote IP must be in the same network."
+                self.add_error("local_ip", msg)
+                self.add_error("remote_ip", msg)
 
 
 class BgpPeeringFilterForm(BootstrapMixin, forms.ModelForm):
