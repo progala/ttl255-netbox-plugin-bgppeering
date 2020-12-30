c80a5e80372ad169bdf9f1906ec12522a7a43493
progala <progala@progala.net>
2020-12-30 11:33:22 +0000

    Add url for delete view

--- netbox_bgppeering/urls.py
+++ netbox_bgppeering/urls.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.urls import path
 
-from .views import BgpPeeringCreateView, BgpPeeringView, BgpPeeringListView
+from .views import (
+    BgpPeeringCreateView,
+    BgpPeeringDeleteView,
+    BgpPeeringListView,
+    BgpPeeringView,
+)
 
 
 urlpatterns = [
     path("", BgpPeeringListView.as_view(), name="bgppeering_list"),
     path("<int:pk>/", BgpPeeringView.as_view(), name="bgppeering"),
     path("add/", BgpPeeringCreateView.as_view(), name="bgppeering_add"),
+    path("<int:pk>/delete/", BgpPeeringDeleteView.as_view(), name="bgppeering_delete"),
 ]
