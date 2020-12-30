5161835fcbb01bbd89908cbdafa338847d52e72e
progala <progala@progala.net>
2020-12-30 11:34:17 +0000

    Add trash can icon class

--- netbox_bgppeering/icon_classes.py
+++ netbox_bgppeering/icon_classes.py
@@ -6,10 +6,12 @@ if NETBOX_RELEASE_CURRENT >= NETBOX_RELEASE_210:
         "plus": "mdi mdi-plus-thick",
         "search": "mdi mdi-magnify",
         "remove": "mdi mdi-close-thick",
+        "trash": "mdi mdi-trash-can-outline",
     }
 else:
     icon_classes = {
         "plus": "fa fa-plus",
         "search": "fa fa-search",
         "remove": "fa fa-remove",
+        "trash": "fa fa-trash",
     }
